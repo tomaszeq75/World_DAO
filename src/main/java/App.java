a647340c9373d43261aa4f7e113ef2c04f123676
Tomasz Wołyniec <t.wolyniec@tlen.pl>
2021-04-23 15:46:42 +0200

    added methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,13 @@ public class App {
         WorldDAO worldDAO = new WorldDAO();
 
         System.out.println(worldDAO.getAll());
-        System.out.println(worldDAO.getByName("San").size());
+        System.out.println(worldDAO.getByName("San A%").size());
+
+        System.out.println("\n By id: " + worldDAO.getById(4070));
+        System.out.println("-------------------------\n" + worldDAO.getByCountryCode("pol"));
+
+        System.out.println("-------------------------\n" + worldDAO.getByPopulationBetween(1000000, 1100000));
+
 
     }
 }
--- src/main/java/DAO/WorldDAO.java
+++ src/main/java/DAO/WorldDAO.java
@@ -3,6 +3,7 @@ package DAO;
 import models.City;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,11 +30,33 @@ public class WorldDAO {
     }
 
     public List<City> getByName(String name) {
-        List<City> items = new ArrayList<>();
         String query = "select * from city where `name` like ?";
+        return getFromQuery(query, name);
+    }
+
+    public City getById(int id) {
+        String query = "select * from city where id = ?";
+        return getFromQuery(query, String.valueOf(id)).get(0);
+    }
+
+    public List<City> getByCountryCode(String code) {
+        String query = "select * from city where CountryCode = ?";
+        return getFromQuery(query, code);
+    }
+
+    public List<City> getByPopulationBetween(int from,  int to) {
+        String query = "select * from city where population between ? and ?";
+        return getFromQuery(query, String.valueOf(from), String.valueOf(to));
+    }
+
+    private List<City> getFromQuery(String query, String... param) {
+        List<City> items = new ArrayList<>();
+
         openConnection();
         try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, name);
+            for (int i = 0; i < param.length; i++) {
+                statement.setString(i + 1, param[i]);
+            }
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 items.add(getCityFromResultset(resultSet));
@@ -44,7 +67,6 @@ public class WorldDAO {
         closeConnection();
         return items;
     }
-
 
     public City getCityFromResultset(ResultSet resultSet) throws SQLException {
         City city = new City();
